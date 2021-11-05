e2501474582ae107b3fb6bde4fbe9924e3ce4c54
Immanuel <57101108+immanuel404@users.noreply.github.com>
2021-11-05 17:29:29 +0200

    Update config.php

--- Estate Properties/login/config.php
+++ Estate Properties/login/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'sql202.epizy.com');
-define('DB_USERNAME', 'epiz_25982299');
-define('DB_PASSWORD', 'gm3B5wyZ6kV');
-define('DB_NAME', 'epiz_25982299_properties');
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
